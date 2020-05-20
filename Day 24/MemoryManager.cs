2caad5ba02074abf778575fccd40c684f9bb10e2
faha-berdiev <madysoftware@gmail.com>
2020-05-20 05:16:52 +0300

    Memory Manager

---
+++ Day 24/MemoryManager.cs
@@ -0,0 +1,24 @@
+﻿namespace Day_24
+{
+    class MemoryManager
+    {
+        private static double memory = 0;
+
+        public static void MemoryClear()
+        {
+            memory = 0;
+        }
+        public static double MemoryRecall(double A)
+        {
+            return memory;
+        }
+        public static void MemoryStore(double A)
+        {
+            memory = A;
+        }
+        public static void MemoryPlus(double A)
+        {
+            memory += A;
+        }
+    }
+}
